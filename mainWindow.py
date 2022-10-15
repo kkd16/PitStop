355975a9ec8bbd6bd05ee57840f24f084571d828
Kyle  Deliyannides <kkd16@sfu.ca>
2022-10-15 13:47:28 -0700

    added timer window

--- mainWindow.py
+++ mainWindow.py
@@ -1,6 +1,15 @@
 # MAIN FRAME WITH GRAPHICS
 import pygame
 
+import time
+import math
+import timeFunctions
+
+#Timer string
+timerString = "00:00"
+timerInt = 0
+tick = 0
+
 ## ALL FUNCTIONS ##
 
 # Image display function
@@ -46,7 +55,7 @@ clock = pygame.time.Clock()
 
 ## OBJECT INITIALIZATION ##
 
-text = font.render('Kyle is gay', True, black, white)
+text = font.render('', True, black, white)
 textRect = text.get_rect()
 textRect.center = (150, 350)
 
@@ -66,7 +75,8 @@ while running:
             # Homescreen Poo button presssed
             if modes==0 and (mousePos[0]>43 and mousePos[0]<343) and (mousePos[1]>50 and mousePos[1]<200):
                 modes=1
-                # Do timer functions maybe
+                timerInt = 0
+                tick = 0
         
     # Update display
 
@@ -78,9 +88,19 @@ while running:
     # In timer
     elif modes==1:
         screen.fill(white)
-        text = font.render('Kyle is gay', True, black, white)
+        text = font.render(timerString, True, black, white)
         screen.blit(text, textRect)
+
+        mins, secs = divmod(timerInt, 60)
+        timerString = '{:02d}:{:02d}'.format(mins, secs)
+        print(timerString, end="\r")
+        #time.sleep(1)
+        tick += 1
+
+        timerInt = math.floor(tick/60)
+
+
         
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(60)
